dbb55ac19f94dc3c51f90cddfeca39802fafab37
723869951@163.com <723869951@163.com>
2020-05-30 01:02:42 +0800

    添加项目文件。

---
+++ Model/timer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProduce1.Model
+{
+    public  static class timer
+    {
+        public  static DateTime Dtn { get; set; }
+
+    }
+}
